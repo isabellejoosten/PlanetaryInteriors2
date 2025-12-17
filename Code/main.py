f1ac0b369cc1a09c653bef39962747f134af8670
tjhgpeeters <t.j.h.g.peeters@student.tudelft.nl>
2025-12-17 15:34:53 +0100

    cleaned up some stuff

--- Code/main.py
+++ Code/main.py
@@ -9,7 +9,7 @@ import functions
 bulkModulus_homogeneous = (p.bulkModulus_core*functions.sphereVolume(p.R_core) + p.bulkModulus_HPI*functions.shellVolume(p.R_core, p.R_HPI) + p.bulkModulus_ocean*functions.shellVolume(p.R_HPI, p.R_ocean) + p.bulkModulus_crust*functions.shellVolume(p.R_ocean, p.R)) / functions.sphereVolume(p.R)
 shearModulus_homogeneous = (p.shearModulus_core*functions.sphereVolume(p.R_core) + p.shearModulus_HPI*functions.shellVolume(p.R_core, p.R_HPI) + p.shearModulus_ocean*functions.shellVolume(p.R_HPI, p.R_ocean)  + p.shearModulus_crust*functions.shellVolume(p.R_ocean, p.R)) / functions.sphereVolume(p.R)
 viscosity_homogeneous = (p.viscosity_core*functions.sphereVolume(p.R_core) + p.viscosity_HPI*functions.shellVolume(p.R_core, p.R_HPI) + p.viscosity_ocean*functions.shellVolume(p.R_HPI, p.R_ocean)  + p.viscosity_crust*functions.shellVolume(p.R_ocean, p.R)) / functions.sphereVolume(p.R)
-layers = [delftide.TidalLayer("Titan", thickness=p.R, density=p.bulkDensity, shear_modulus=shearModulus_homogeneous, viscosity=viscosity_homogeneous)]
+layers = [delftide.TidalLayer("Titan", thickness=p.R, density=p.bulkDensity, shear_modulus=shearModulus_homogeneous, rheology=rheologies.ElasticRheology())]
 omega = 4.56e-6
 model = delftide.TidalInterior("Homogeneous Titan", layers)
 tide = delftide.TidalResponse(model, omega)
@@ -20,16 +20,16 @@ k2_verification = functions.k2_analytical(shearModulus_homogeneous, p.bulkDensit
 print("k2 calculated analytically: ", k2_verification)
 #print(float(tide.k2.real))
 k2error_real = (float(k2_verification.real) - float(tide.k2.real)) / float(tide.k2.real) * 100
-k2error_im = (float(k2_verification.imag) - float(tide.k2.imag)) / float(tide.k2.imag) * 100
+#k2error_im = (float(k2_verification.imag) - float(tide.k2.imag)) / float(tide.k2.imag) * 100
 print("k2 error (real part): ", k2error_real, "%")
-print("k2 error (imaginary part): ", k2error_im, "%")
+#print("k2 error (imaginary part): ", k2error_im, "%")
 
 h2_verification = functions.h2_analytical(shearModulus_homogeneous, p.bulkDensity, p.g, p.R, viscosity_homogeneous, omega)
 print("h2 calculated analytically: ", k2_verification)
 h2error_real = (float(h2_verification.real) - float(tide.h2.real)) / float(tide.h2.real) * 100
-h2error_im = (float(h2_verification.imag) - float(tide.h2.imag)) / float(tide.h2.imag) * 100
+#h2error_im = (float(h2_verification.imag) - float(tide.h2.imag)) / float(tide.h2.imag) * 100
 print("h2 error (real part): ", h2error_real, "%")
-print("h2 error (imaginary part): ", h2error_im, "%")
+#print("h2 error (imaginary part): ", h2error_im, "%")
 
 print("Averaged bulk modulus: ", bulkModulus_homogeneous/1000000000, " GPa")
 print("Averaged shear modulus: ", shearModulus_homogeneous/1000000000, " GPa")
