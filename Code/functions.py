5c82c427b2d0cf1ddf06c70636e692910cf9c4cc
tjhgpeeters <t.j.h.g.peeters@student.tudelft.nl>
2025-12-17 14:56:06 +0100

    Added H_2 analytical expression

--- Code/functions.py
+++ Code/functions.py
@@ -6,14 +6,15 @@ def sphereVolume(radius):
 def shellVolume(innerRadius, outerRadius):
     return sphereVolume(outerRadius) - sphereVolume(innerRadius)
 
-def analytical_k_n(n,mu,bulkDensity,g,R):
-    mu_eff_hat = mu / (bulkDensity * g * R)
-    mu_n_hat = (2*n**2+4*n+3)/n * mu_eff_hat
-    k_n = 2 / (2*(n-1)) * (1 / (1 + mu_n_hat))
-    return k_n
+
 
 def k2_analytical(mu, rho, g, radius, viscosity, omega):
     a = mu/(rho*g*radius)
     b = 1/(1-(complex(0, mu))/(viscosity*omega))
 
     return 3/2*1/(1+(19/2)*a*b)
+
+def h2_analytical(mu, rho, g, radius, viscosity, omega):
+    a = mu/(rho*g*radius)
+    b = 1/(1-(complex(0, mu))/(viscosity*omega))
+    return 5/2*1/(1+(19/2)*a*b)
