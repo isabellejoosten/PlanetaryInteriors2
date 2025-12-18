1c13123927f5f2cb780d78aae327749ab0c7fbcb
tjhgpeeters <t.j.h.g.peeters@student.tudelft.nl>
2025-12-18 14:31:28 +0100

    Clean up and added the multi layer model

--- Code/main.py
+++ Code/main.py
@@ -21,50 +21,24 @@ viscosity_homogeneous = (p.viscosity_core*functions.sphereVolume(p.R_core) + p.v
 range_shearModulus = np.arange(0.1*shearModulus_homogeneous, 10.1*shearModulus_homogeneous, 0.001*shearModulus_homogeneous)
 range_viscosity = np.arange(10.0e-5*viscosity_homogeneous, 10.0e5*viscosity_homogeneous, 100.0*viscosity_homogeneous)
 
-# Single-layer model - viscoelastic
-layers = [delftide.TidalLayer("Titan", thickness=p.R, density=p.bulkDensity, shear_modulus=shearModulus_homogeneous, viscosity=viscosity_homogeneous)]
-omega = 4.56e-6
-model = delftide.TidalInterior("Homogeneous Titan", layers)
-tide = delftide.TidalResponse(model, omega)
-tide.plot()
-print(tide)
 
-# Single-layer model - viscoelastic
-layers = [delftide.TidalLayer("Titan", thickness=p.R, density=p.bulkDensity, shear_modulus=shearModulus_homogeneous, viscosity=viscosity_homogeneous)]
-omega = 4.56e-6
-model = delftide.TidalInterior("Homogeneous Titan", layers)
-tide = delftide.TidalResponse(model, omega)
-tide.plot()
-print(tide)
+R_core,density_core,density_ocean = functions.MultiLayerSolver()
 
-k2_verification = functions.k2_analytical(shearModulus_homogeneous, p.bulkDensity, p.g, p.R, viscosity_homogeneous, omega)
-print("k2 calculated analytically: ", k2_verification)
-#print(float(tide.k2.real))
-k2error_real = (float(k2_verification.real) - float(tide.k2.real)) / float(tide.k2.real) * 100
-#k2error_im = (float(k2_verification.imag) - float(tide.k2.imag)) / float(tide.k2.imag) * 100
-print("k2 error (real part): ", k2error_real, "%")
-#print("k2 error (imaginary part): ", k2error_im, "%")
-
-h2_verification = functions.h2_analytical(shearModulus_homogeneous, p.bulkDensity, p.g, p.R, viscosity_homogeneous, omega)
-print("h2 calculated analytically: ", k2_verification)
-h2error_real = (float(h2_verification.real) - float(tide.h2.real)) / float(tide.h2.real) * 100
-#h2error_im = (float(h2_verification.imag) - float(tide.h2.imag)) / float(tide.h2.imag) * 100
-print("h2 error (real part): ", h2error_real, "%")
-#print("h2 error (imaginary part): ", h2error_im, "%")
-
-R_core, density_core, density_ocean = sympy.symbols('R_core, density_core, density_ocean')
-eq1 = sympy.Eq(functions.totalRadius(R_core, p.R_HPI, p.R_ocean, p.R_ocean),p.R)
-eq2 = sympy.Eq(functions.bulkdensity(R_core, density_core, p.R_HPI, p.density_HPI, p.R_ocean, density_ocean, p.R_crust, p.density_crust),p.bulkDensity)
-eq3 = sympy.Eq(functions.MomentOfInertiaPlanet(R_core, density_core, p.R_HPI, p.density_HPI, p.R_ocean, density_ocean, p.R_crust, p.density_crust),p.MoI_factor*p.R**2*functions.sphereVolume(p.R)*p.bulkDensity)
-
-solution = sympy.solve((eq1,eq2,eq3),(R_core,density_core,density_ocean))
-print(solution)
+print("Radius of the core is ", np.floor(R_core/10**3) , 'km')
+print("Density of the core is ", np.floor(density_core) , 'kg/m^3')
+print("Density of the ocean is ", np.floor(density_ocean) , 'kg/m^3')
 
 
+'''
 print("Averaged bulk modulus: ", bulkModulus_homogeneous/1000000000, " GPa")
 print("Averaged shear modulus: ", shearModulus_homogeneous/1000000000, " GPa")
 print("Averaged viscosity: ", viscosity_homogeneous, " Pa*s")
+'''
 
-tide_elastic = functions.singleLayerTitan(p.R, p.bulkDensity, shearModulus_homogeneous, viscosity_homogeneous, "e")
-tide_viscoelastic = functions.singleLayerTitan(p.R, p.bulkDensity, shearModulus_homogeneous, viscosity_homogeneous, "v")
-tide_liquid = functions.singleLayerTitan(p.R, p.bulkDensity, shearModulus_homogeneous, viscosity_homogeneous, "l")
+#tide_elastic = functions.singleLayerTitan(p.R, p.bulkDensity, shearModulus_homogeneous, viscosity_homogeneous, "e")
+#tide_viscoelastic = functions.singleLayerTitan(p.R, p.bulkDensity, shearModulus_homogeneous, viscosity_homogeneous, "v")
+#tide_liquid = functions.singleLayerTitan(p.R, p.bulkDensity, shearModulus_homogeneous, viscosity_homogeneous, "l")
+
+
+tide_multilayer = functions.multiLayerTitan()
+titan = delftide.TidalResponse.examples.titan()
